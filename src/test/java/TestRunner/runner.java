b9db63550f09d7b186e1c015e05c4959fd7901e3
binnyjayakumar <157058767+binnyjayakumar@users.noreply.github.com>
2024-03-24 13:02:45 -0400

    Update runner.java
    Runner

--- src/test/java/TestRunner/runner.java
+++ src/test/java/TestRunner/runner.java
@@ -10,9 +10,9 @@ public class runner
 			glue = {"BookingAPI_Stepdefinitions"},
 			//tags ="@smoke",
 			monochrome = true,
-			plugin = "pretty","io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
-  				"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
-				"html:target/HtmlReport/htmlreport.html")
+			plugin = {"pretty","io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
+					  	"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+			"html:target/HtmlReport/htmlreport.html"})
 
 	public class TestRunner extends AbstractTestNGCucumberTests  {
 
